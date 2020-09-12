f7368cbcd504ce1dce3e595f106fc3ed18756660
Cynthia <bbliu666@126.com>
2020-09-12 21:57:12 +0800

    commit

--- 3. basic algorithm/chapter 02/chatroom/client1.py
+++ 3. basic algorithm/chapter 02/chatroom/client1.py
@@ -7,53 +7,54 @@
 import sys
 import socket
 import threading
+
 from tkinter import *
-from tkinter.ttk import *  # ttk比tk更好看, 同名组件优先用ttk的
+from tkinter.ttk import *
+
 from tkinter.messagebox import *
 from tkinter.scrolledtext import ScrolledText
 
-root = Tk()
-root.geometry("+600+300")
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 class LoginFrame:
     def __init__(self, parent, f1, f2):
-        # 自定义组件里的东西最好用一个frame包起来, 便于统一操作
+
+        self._target = None
+        # 自定义组件里最好用一个frame包起来, 便于统一操作
         self._frame = Frame(parent)
 
         Label(self._frame, text="Host: ").grid(row=1, column=1, columnspan=2)
         self._host = Entry(self._frame)
+        self._host.insert(END, socket.gethostname()+":8088")
         self._host.grid(row=1, column=3, columnspan=3)
 
         Label(self._frame, text="ID: ").grid(row=2, column=1, columnspan=2)
         self._id = Entry(self._frame)
+        self._id.insert(END, '158')
         self._id.grid(row=2, column=3, columnspan=3)
 
-        Button(self._frame, text="退出", command=f1).grid(row=3, column=2, columnspan=2, pady=15)
-        Button(self._frame, text="登陆", command=f2).grid(row=3, column=4, columnspan=1, pady=15)
-
-    def grid(self, *args, **kwargs):
-        self._frame.grid(*args, **kwargs)
+        Button(self._frame, text='退出', command=lambda : f1(self)).grid(row=3, column=2, columnspan=2, pady=15)
+        Button(self._frame, text='登陆', command=lambda : f2(self, self._target)).grid(row=3, column=4, columnspan=1, pady=15)
 
     def pack(self, *args, **kwargs):
         self._frame.pack(*args, **kwargs)
-
-    def grid_forget(self):
-        self._frame.grid_forget()
 
     def pack_forget(self):
         self._frame.pack_forget()
 
     def get_host(self):
-        # host, port = self._host.get().split(':')
-        # return host, int(port)
-        return socket.gethostname(), 8088
+        host, port = self._host.get().split(':')
+        return host, int(port)
 
     def get_id(self):
         return self._id.get()
 
+    def set_target(self, target):
+        self._target = target
+
 class ChatFrame:
-    def __init__(self, parent):
+    def __init__(self, parent, thread, f):
+
+        self._thread = thread
+
         self._frame = Frame(parent)
         self._text = ScrolledText(self._frame, state=DISABLED, width=50, height=20)
         self._text.pack()
@@ -64,16 +65,10 @@ class ChatFrame:
         self._message = Entry(self._frame, width=25)
         self._message.pack(side=LEFT)
 
-        Button(self._frame, text='发送', command=self.send_message).pack(side=RIGHT)
-
-    def send_message(self):
-        target = self._target.get()
-        message = self._message.get()
-        self.add_content("->"+target+": "+message+'\n')
-        client.send((target+":"+message).encode('utf-8'))
+        Button(self._frame, text='发送', command=lambda : f(self)).pack(side=RIGHT)
 
     def clear_content(self):
-        # DISABLED状态不光用户不能编辑, delete, insert也无效...
+        # DISABLED状态不光用户不能编辑, delete, insert也无效
         self._text.configure(state=NORMAL)
         self._text.delete(1.0, END)
         self._text.configure(state=DISABLED)
@@ -92,58 +87,58 @@ class ChatFrame:
     def pack_forget(self):
         self._frame.pack_forget()
 
-class RecvThread(threading.Thread):
-    def __init__(self, f):
-        threading.Thread.__init__(self)
-        self._f = f
+    def listen(self):
+        thread = self._thread(self)
+        thread.setDaemon(True)
+        thread.start()
 
-    def run(self) -> None:
-        self._f()
+root = Tk()
+root.geometry("+600+300")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def f1():
+def login_f1(self: LoginFrame):
     root.quit()
 
-# 函数体里面的变量可以先写后声明, 比如这里的login_frame
-# 除了这么写, 还可以给个self参数, 子组件类调方法的时候把自身self传进去
-def f2():
-    host, port = login_frame.get_host()
+def login_f2(self: LoginFrame, target: ChatFrame):
+    host, port = self.get_host()
 
     try:
         client.connect((host, port))
     except OSError:
-        showerror("提示", "\n连接失败, 请检查服务器地址!")
+        showerror("提示", "\n连接失败!")
         return
 
-    client.send(login_frame.get_id().encode('utf-8'))
+    client.send(self.get_id().encode('utf-8'))
 
-    def f():
+    self.pack_forget()
+    target.pack()
+    target.listen()
+
+class RecvThread(threading.Thread):
+    def __init__(self, chatroom: ChatFrame):
+        threading.Thread.__init__(self)
+        self._chatroom = chatroom
+
+    def run(self) -> None:
         while True:
             data = client.recv(1024)
             if data:
-                origin, message = data.decode("utf-8").split(':')
-                chat_frame.add_content(origin+": "+message+'\n')
+                origin, message = data.decode('utf-8').split(':')
+                self._chatroom.add_content(origin+': '+message+'\n')
             else:
-                showerror("提示", "\n服务器断开连接, 点击退出!")
+                showerror('提示', '\n服务器断开连接')
                 sys.exit()
 
-    # 注意, f到RecvThread里, 执行的时候上下文关系还是原f所在的上下文
-    # 所以这里f中的chat_frame变量虽然不是全局变量, 但也是能找到的
-    thread = RecvThread(f)
-    thread.setDaemon(True)
-    thread.start()
+def chat_f(self: ChatFrame):
+    target = self._target.get()
+    message = self._message.get()
+    self.add_content("->"+target+": "+message+"\n")
+    client.send((target+":"+message).encode('utf-8'))
 
-    login_frame.pack_forget()
-
-    chat_frame = ChatFrame(root)
-    chat_frame.pack()
-
-login_frame = LoginFrame(root, f1, f2)
-
+login_frame = LoginFrame(root, login_f1, login_f2)
 login_frame.pack()
-login_frame.pack_forget()
-
-chat_frame = ChatFrame(root)
-chat_frame.pack()
+chat_frame = ChatFrame(root, RecvThread, chat_f)
+login_frame.set_target(chat_frame)
 
 root.mainloop()
 
--- 3. basic algorithm/chapter 02/chatroom/server.py
+++ 3. basic algorithm/chapter 02/chatroom/server.py
@@ -1,57 +1,65 @@
 # -*- coding: utf-8 -*-
 # Author: Cynthia
+
+"""
+    builtinf演示UI组件内部实现控制逻辑
+"""
+
 """
     服务端不需要界面
-    主线程接受来自客户端的连接, 最大10
+    主程序接受来自客户端的连接, 最大10
     支线程接受来自客户端的信息, 信息格式ID:message
-    首次连接成功后要单独把ID发过来
+    首次连接成功后要单独把ID发过来    
 """
-
 import socket
 import weakref
+
 import threading
 
-# AF_INET用于跨机器通信; SOCK_STREAM表示面向连接
+# AF_INET用于跨机器通信, SOCK_STREAM表示面向连接
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host, port = socket.gethostname(), 8088
 server.bind((host, port))
 
 server.listen(10)  # 最大连接数10
+
 print("server start!")
 print("==============================")
 
 class RecvThread(threading.Thread):
-
     id2cs = weakref.WeakValueDictionary()
 
     def __init__(self, id, cs, ca):
-        # 这里不能用super()
-        threading.Thread.__init__(self)
-        self._id = id  # 用户id
-        self._cs = cs  # 当前socket
-        self._ca = ca  # 用户ip,端口
+        threading.Thread.__init__(self)  # 这里不能用super()
 
-    def run(self):
+        self._id = id  # 用户ID
+        self._cs = cs  # 当前socket
+        self._ca = ca  # 用户ip, 端口
+
+    def run(self) -> None:
         while True:
             data = self._cs.recv(1024)
             if not data:
                 print("{}:{} disconnected".format(self._ca[0], self._ca[1]))
                 self._cs.close()
-                break
+                break  # return 也行
             data = data.decode('utf-8')
+
             if ":" not in data: continue  # 不合法的信息直接丢失
-            target, message = data.split(":")
-            if target not in RecvThread.id2cs: continue  # 无效的目标用户则信息直接丢失
+            target, message = data.split(':')
+            if target not in RecvThread.id2cs: continue  # 无效的目标用户则信息也直接丢失
             RecvThread.id2cs[target].send((self._id+":"+message).encode('utf-8'))
 
 while True:
     cs, ca = server.accept()
-    print("connected from {}:{}".format(ca[0], ca[1]))
+    print('connected from {}:{}'.format(ca[0], ca[1]), end="")
     id = cs.recv(1024).decode('utf-8')
-    print(id)
+    print(", {}".format(id))
     RecvThread.id2cs[id] = cs
 
     thread = RecvThread(id, cs, ca)
     thread.setDaemon(True)
     thread.start()
+
+
