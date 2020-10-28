542c91cac29594997c717dc3f4e85f2e76c3ea1b
Cynthia <bbliu666@126.com>
2020-10-28 14:25:45 +0800

    commit

---
+++ 3. basic algorithm/chapter 04/homework/solution-26.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Author: Cynthia
+
+"""
+    汉诺塔
+"""
+
+def f1(n):
+
+    def rf(k, f, b, t):
+        """
+
+        :param k: 几个盘子
+        :param f: 从哪
+        :param b: 经过哪
+        :param t: 移动到哪
+        :return:
+        """
+
+    rf(n, 'A', 'B', 'C')
+
+f1(3)
