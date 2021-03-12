ee75a48fe1b9b9fffe202a2dcbcb340369999150
Cynthia <bbliu666@126.com>
2021-03-12 17:06:18 +0800

    commit

--- 3. basic algorithm/chapter 07/homework/solution-21.py
+++ 3. basic algorithm/chapter 07/homework/solution-21.py
@@ -2,8 +2,19 @@
 # Author: Cynthia
 
 """
-    逆序实现Move-to-Front收藏夹
-    (即每次访问完移动到末尾, 查找也从末尾开始)
+    Move-to-Front收藏夹, n个元素
+    设计长度为n^2的访问序列, 使得时间复杂度为O(n^3)
+
+    分析:
+    总时间复杂度n^3, 那么每个元素访问时间复杂度O(n)
+
+    1-2-3-4-5, 以左为头, 那么访问顺序为:
+    5: 5-1-2-3-4
+    4: 4-5-1-2-3
+    3...
+
+    即按最近一次访问时间计, 每次都去访问最早的那个元素
+
 """
 
 class IllegalNode(Exception):
@@ -15,6 +26,7 @@ class IllegalPosition(Exception):
 class DoubleLinkedList:
 
     class Node:
+
         __slots__ = 'e', 'left', 'right'
 
         def __init__(self, e=None):
@@ -23,6 +35,7 @@ class DoubleLinkedList:
             self.right = None
 
     def __init__(self):
+
         self.head = self.Node()
         self.tail = self.Node()
 
@@ -49,11 +62,9 @@ class DoubleLinkedList:
         if not self._validate(right):
             raise IllegalNode
 
-        if right is self.head:
-            raise IllegalNode
+        node = self.Node(e)
 
         left = right.left
-        node = self.Node(e)
 
         node.left, node.right = left, right
         left.right, right.left = node, node
@@ -63,15 +74,11 @@ class DoubleLinkedList:
         return node
 
     def _add_after(self, left, e):
-
         if not self._validate(left):
             raise IllegalNode
 
-        if left is self.tail:
-            raise IllegalNode
-
-        right = left.right
         node = self.Node(e)
+        right = left.right
 
         node.left, node.right = left, right
         left.right, right.left = node, node
@@ -91,26 +98,19 @@ class DoubleLinkedList:
         node.left.right = node.right
         node.right.left = node.left
 
-        r = node.e
-
-        node.left = node.right = node.e = None
-
         self.nodes.remove(node)
 
-        return r
+        return node.e
 
-    def _move_last(self, node):
+    def _move_first(self, node):
 
         if not self._validate(node):
-            raise IllegalNode
-
-        if node in [self.head, self.tail]:
             raise IllegalNode
 
         node.left.right = node.right
         node.right.left = node.left
 
-        left, right = self.tail.left, self.tail
+        left, right = self.head, self.head.right
 
         node.left, node.right = left, right
         left.right, right.left = node, node
@@ -120,8 +120,8 @@ class DoubleLinkedList:
 class PositionLinkedList(DoubleLinkedList):
 
     class Position:
-        def __init__(self, container, node):
 
+        def __init__(self, container, node):
             self.container = container
             self.node = node
 
@@ -139,7 +139,6 @@ class PositionLinkedList(DoubleLinkedList):
         return True
 
     def n2p(self, node):
-
         if not self._validate(node):
             raise IllegalNode
 
@@ -157,13 +156,13 @@ class PositionLinkedList(DoubleLinkedList):
 
     def before(self, p):
         if not self.validate(p):
-            raise IllegalNode
+            raise IllegalPosition
 
         return self.n2p(p.node.left)
 
     def after(self, p):
         if not self.validate(p):
-            raise IllegalNode
+            raise IllegalPosition
 
         return self.n2p(p.node.right)
 
@@ -182,6 +181,7 @@ class PositionLinkedList(DoubleLinkedList):
         return self.n2p(self._add_after(self.head, e))
 
     def add_last(self, e):
+
         return self.n2p(self._add_before(self.tail, e))
 
     def add_before(self, p, e):
@@ -198,6 +198,7 @@ class PositionLinkedList(DoubleLinkedList):
         return self._add_after(p.node, e)
 
     def delete(self, p):
+
         if not self.validate(p):
             raise IllegalPosition
 
@@ -207,11 +208,12 @@ class PositionLinkedList(DoubleLinkedList):
 
         return r
 
-    def move_last(self, p):
+    def move_first(self, p):
+
         if not self.validate(p):
             raise IllegalPosition
 
-        self._move_last(p.node)
+        self._move_first(p.node)
 
 class FavoritesListMTF:
 
@@ -219,7 +221,8 @@ class FavoritesListMTF:
 
         __slots__ = 'v', 'c'
 
-        def __init__(self, v, c=1):
+        def __init__(self, v=None, c=1):
+
             self.v = v
             self.c = c
 
@@ -236,20 +239,6 @@ class FavoritesListMTF:
 
         for e in self.data:
             yield e.v, e.c
-    """
-    def find(self, v):
-        # 逆序, 从末尾开始查找, 返回data的位置对象
-        cursor = self.data.last()
-
-        while cursor:
-
-            if cursor.e.v is v:
-                return cursor
-
-            cursor = self.data.before(cursor)
-
-        return None
-    """
 
     def find(self, v):
 
@@ -259,13 +248,13 @@ class FavoritesListMTF:
             if p.e.v is v:
                 return p
             else:
-                return rf(self.data.before(p))
+                return rf(self.data.after(p))
 
-        return rf(self.data.last())
+        return rf(self.data.first())
 
     def move(self, p):
-        # 逆序, 移动到末尾
-        self.data.move_last(p)
+
+        self.data.move_first(p)
 
     def access(self, v):
 
@@ -273,13 +262,12 @@ class FavoritesListMTF:
 
         if p:
             p.e.c += 1
+            self.move(p)
 
         else:
             e = self.ElementType(v)
 
-            p = self.data.add_first(e)
-
-        self.move(p)
+            self.data.add_first(e)
 
     def remove(self, v):
 
@@ -289,10 +277,12 @@ class FavoritesListMTF:
             self.data.delete(p)
 
     def top(self, k):
+
         if not 1 <= k <= len(self):
             raise KeyError
 
         t = list(self)
+
         t.sort(key=lambda x:x[1], reverse=True)
 
         return t[:k]
