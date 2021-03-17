48323f0be0eaf2700803a949dc94e48475c97c67
Cynthia <bbliu666@126.com>
2021-03-17 16:48:00 +0800

    commit

--- 3. basic algorithm/chapter 07/homework/solution-25.py
+++ 3. basic algorithm/chapter 07/homework/solution-25.py
@@ -2,74 +2,83 @@
 # Author: Cynthia
 
 """
-    利用带头结点的单向链表实现栈
+    利用带头结点的单向链表实现列表
 """
+
 class EmptyError(Exception):
     pass
 
-class Stack:
+class Queue:
 
     class Node:
 
         __slots__ = 'e', 'next'
 
         def __init__(self, e=None):
+
             self.e = e
             self.next = None
 
     def __init__(self):
-
         self.head = self.Node()
         self.count = 0
 
     def __len__(self):
         return self.count
 
-    def empty(self):
-        return len(self) == 0
-
     def __iter__(self):
+
         cursor = self.head.next
 
         while cursor:
             yield cursor.e
+
             cursor = cursor.next
 
-    def push(self, e):
-        node = self.Node(e)
-        node.next = self.head.next
-        self.head.next = node
+    def empty(self):
+        return self.count == 0
 
+    def enqueue(self, e):
+
+        cursor = self.head
+
+        for _ in range(self.count):
+            cursor = cursor.next
+
+        node = self.Node(e)
+
+        cursor.next = node
         self.count += 1
 
         return node
 
-    def pop(self):
+    def dequeue(self):
         if self.empty():
             raise EmptyError
 
         r = self.head.next
 
         self.head.next = r.next
-
         self.count -= 1
 
         return r.e
 
-    def top(self):
+    def first(self):
         if self.empty():
             raise EmptyError
 
         return self.head.next.e
 
-s = Stack()
-s.push(1)
-s.push(2)
-s.push(-1)
-s.push(8)
-print(list(s))
-s.pop()
-print(list(s))
-print(s.top())
-print(s.count)
-print(s.empty())
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(8)
+print(list(q))
+print(q.count)
+print(q.first())
+q.dequeue()
+print(list(q))
+print(q.count)
+print(q.first())
